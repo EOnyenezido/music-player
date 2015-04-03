ffe4c52e486f7863d0994198db44f10276f62117
EOnyenezido <sleonvictor@gmail.com>
2015-04-03 20:40:47 +0100

    modals not working

---
+++ script.js
@@ -0,0 +1,19 @@
+/*DZ.init({
+		appId  : '154751',
+		channelUrl : 'http://external.codecademy.com/channel.html',
+		/*player : {
+			container: 'player',
+			width : 600,
+			height : 300,
+			format : 'vertical',
+			onload : function(player){
+				player.volume = 200;
+			}
+		}
+});
+
+
+
+var ike = document.createElement("p");*/
+
+//ike.innerHTML = emeka;
